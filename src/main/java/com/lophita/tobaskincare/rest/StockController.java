6bf4b8e1576e23b57dfad89c3e281c4dd5d7dfc6
lophita <lophita@fabelio.com>
2021-12-09 17:13:53 +0700

    swagger configuration

--- src/main/java/com/lophita/tobaskincare/rest/StockController.java
+++ src/main/java/com/lophita/tobaskincare/rest/StockController.java
@@ -2,23 +2,32 @@ package com.lophita.tobaskincare.rest;
 
 import com.lophita.tobaskincare.dto.StockDto;
 import com.lophita.tobaskincare.service.StockService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("stock")
+@Api(tags = "Stock")
+@RequestMapping("/stock")
 public class StockController {
 
     @Autowired
     private StockService stockService;
 
+    @RequestMapping(value = "/custom", method = RequestMethod.POST)
+    public String custom() {
+        return "custom";
+    }
+
     @GetMapping("/")
     public List<StockDto> getAllStock() {
 //        return stockService.findAllStock();
-        return null;
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/lophita/tobaskincare/rest/TransactionController.java
+++ src/main/java/com/lophita/tobaskincare/rest/TransactionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("transaction")
+@RequestMapping("/transaction")
 public class TransactionController {
 
     @Autowired
